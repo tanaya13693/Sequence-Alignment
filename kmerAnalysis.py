d29cf2b01ae079b08f8eaf82ddbe7fd842b0153d
Tanaya Vadgave <tanaya13693@gmail.com>
2017-01-15 21:26:14 -0800

    added working code for kmer analysis

---
+++ kmerAnalysis.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+def kmer_list(dna, k):
+    result = []
+    for x in range(len(dna)+1-k):
+        result.append(dna[x:x+k])
+    return result
+
+from Bio import SeqIO
+
+fasta_sequences = SeqIO.parse(open("humanMouse.fasta"),'fasta')
+with open("outputHumanMouse.txt") as out_file:
+    for fasta in fasta_sequences:
+        name, sequence = fasta.id, fasta.seq.tostring()
+
+import collections		
+my_list = kmer_list(sequence, 4)
+c = collections.Counter(my_list)
+print(c.most_common(256))  #64 because total 3mer combinations in DNA sequence
+#write_fasta("outputHumanMouse.txt")
